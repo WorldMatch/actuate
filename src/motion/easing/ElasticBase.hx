35c10008b966c1bc079b665a7e364e74ef873bad
Pierpaolo <kaldais@gmail.com>
2021-08-12 15:50:06 +0200

    Added ElasticBase class

---
+++ src/motion/easing/ElasticBase.hx
@@ -0,0 +1,120 @@
+package motion.easing;
+
+
+class ElasticBase
+{
+	static public var easeIn (get, never):IEasing;
+	static public var easeInOut (get, never):IEasing;
+	static public var easeOut (get, never):IEasing;
+	
+	private static function get_easeIn ():IEasing
+	{
+		return new ElasticEaseIn (1, 1);
+	}
+	
+	private static function get_easeInOut ():IEasing
+	{
+		return new ElasticEaseInOut (1, 1);
+	}
+	
+	private static function get_easeOut ():IEasing
+	{
+		return new ElasticEaseOut (1, 1);
+	}
+}
+
+
+class ElasticEaseIn implements IEasing
+{
+	public var a:Float;
+	public var p:Float;
+	
+	public function new (a:Float, p:Float)
+	{
+		this.a = a;
+		this.p = p;
+	}
+	
+	public function calculate (k:Float):Float
+	{
+		if (k == 0)
+			return 0;
+			
+		if (k == 1)
+			return 1;
+			
+		var s:Float;
+		if (a < 1)
+		{
+			a = 1;
+			s = p / 4;
+		}
+		else
+			s = p / (2 * Math.PI) * Math.asin (1 / a);
+			
+		return -(a * Math.pow(2, 10 * (k -= 1)) * Math.sin( (k - s) * (2 * Math.PI) / p ));
+	}
+}
+
+
+class ElasticEaseInOut implements IEasing
+{
+	public var a:Float;
+	public var p:Float;
+	
+	public function new (a:Float, p:Float)
+	{
+		this.a = a;
+		this.p = p;
+	}
+	
+	public function calculate (k:Float):Float
+	{
+		if (k == 0)
+			return 0;
+			
+		if ((k /= 1 / 2) == 2)
+			return 1;
+		
+		var p:Float = (0.3 * 1.5);
+		var a:Float = 1;
+		var s:Float = p / 4;
+		
+		return (k < 1)
+			? -0.5 * (Math.pow(2, 10 * (k -= 1)) * Math.sin((k - s) * (2 * Math.PI) / p))
+			: Math.pow(2, -10 * (k -= 1)) * Math.sin((k - s) * (2 * Math.PI) / p) * 0.5 + 1;
+	}
+}
+
+
+class ElasticEaseOut implements IEasing
+{
+	public var a:Float;
+	public var p:Float;
+	
+	public function new (a:Float, p:Float)
+	{
+		this.a = a;
+		this.p = p;
+	}
+	
+	public function calculate (k:Float):Float
+	{
+		if (k == 0)
+			return 0;
+			
+		if (k == 1)
+			return 1;
+			
+		var s:Float;
+		if (a < 1)
+		{
+			a = 1;
+			s = p / 4;
+		}
+		else
+			s = p / (2 * Math.PI) * Math.asin (1 / a);
+			
+		return (a * Math.pow(2, -10 * k) * Math.sin((k - s) * (2 * Math.PI) / p ) + 1);
+	}
+}
